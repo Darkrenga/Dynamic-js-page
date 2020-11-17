dacb1dc9fd0a911ffa6502fb9c840d7aed61374d
Daniel Jensen <45940133+Darkrenga@users.noreply.github.com>
2020-11-17 12:14:02 +0100

    Add files via upload

--- Public/JS/app.js
+++ Public/JS/app.js
@@ -1,14 +1,112 @@
-let contentWrapper = document.getElementById("main");
+//GLOBAL VARIABLES
+const YOURWRAPPERIDHERE = "main"
 
-function linkHandler() {
+const CONTENTWRAPPER = document.getElementById(YOURWRAPPERIDHERE);
+let myUl = document.getElementById("myUl");
+let headerTag;
+let contentTag
+let j = -1;
+
+
+//==============================   Calling my functions   ==============================//
+navCreation();
+checkForLandingPage();
+
+
+// Creates the nav bar
+//Works totally fine, creates the navbar and everything in it. 
+//Just need to work out how im gonna change out the pages at the onclick
+
+
+//The only thing you can edit in this function is what happens 
+//on the click, currently it calls a build function of the page
+//you wanted to go to
+function navCreation() {
     for (const link of links) {
-        let htmlLink = document.getElementById(link.linkName);
-        htmlLink.onclick = function() {
-            contentWrapper.innerHTML = link.replaceWith;
+        let li = document.createElement("li");
+        li.classList.add("navList");
+        j++;
+        let anchor = document.createElement("a");
+        anchor.setAttribute("id", link.linkInfo.linkName);
+        anchor.setAttribute("value", j);
+        anchor.innerHTML = link.linkInfo.linkText;
+        anchor.onclick = function() {
+            let indexInArray = this.getAttribute("value");
+            contentCreation(indexInArray);
+        }
+        
+        myUl.appendChild(li);
+        li.appendChild(anchor);
+    }
+}
+
+
+
+
+//On load this function is called and it finds the {  }  with landingPage set to true
+//Dont change anything here as it only calls the creation of landingpage
+let i = -1;
+function checkForLandingPage() {
+    for (const item of links) {
+        i++;
+        if(item.landingPage) {
+            contentCreation(i);
         }
     }
 }
 
-linkHandler();
+
+//Creates the content of a certain page
+//Also creates my view
+function contentCreation(index) {
+    let item = links[index];
+    let page = `<div class="pageWrapper">`
+    checkForHeadingTag(item.title.format);
+    page += `<${headerTag} class="header">${item.title.content}</${headerTag}>`
+
+    page += `</div>`
+    CONTENTWRAPPER.innerHTML = page;
+}
 
 
+
+/**
+ * Checks for:
+ * 
+ * h1
+ * 
+ * h2
+ * 
+ * anything else then that and it returns null
+ * 
+ * @param {obj} titleFormat Takes the path to the thing you want checked
+ */
+function checkForHeadingTag(titleFormat) {
+    if(titleFormat === "Header 1") {
+        headerTag = "h1";
+    } else if (titleFormat === "Header 2") {
+        headerTag = "h2";
+    } else if (titleFormat === "Header 3") {
+        headerTag = "h3";
+    } else {
+        headerTag = null; // Sets it at null if there isnt a viable header
+    }
+    return headerTag;
+}
+
+
+
+function tagFormatting(titleFormat) {
+    if(titleFormat === "Header 1") {
+        contentTag = "h1";
+    } else if (titleFormat === "Header 2") {
+        contentTag = "h2";
+    } else if(titleFormat === "Header 3") {
+
+    }else if(titleFormat === "Paragraph") {
+        contentTag = "p";
+    } else {
+        contentTag = null; // Sets it at null if there isnt a viable header
+    }
+    return contentTag;
+}
--- Public/JS/linkSwitch.js
+++ Public/JS/linkSwitch.js
@@ -1,18 +1,50 @@
 let links = [
-	{
-        linkName: "home",
-        replaceWith: landingPage
+    {
+        landingPage: true,
+        contentType: "page",
+        linkInfo: {
+            linkName: "home",
+            linkText: "Home"
+        },
+        title: {
+            format: "Header 1",
+            content: "HomePage"
+        },
+        subHeader: {
+            format: "Paragraph",
+            content: "I'm the subheader of the landing page",
+            
+        }
+        
     },
     {
-        linkName: "shop",
-        replaceWith: shop
+        contentType: "page",
+        linkInfo: {
+            linkName: "shop",
+            linkText: "Shop"
+        },
+        title: {
+            format: "Header 1",
+            content: "Shopping page"
+        },
+        text: {
+            format: "Header 2",
+            content: "Welcome to the shopping page "
+        }
     },
     {
-        linkName: "om-os",
-        replaceWith: about
+        contentType: "page",
+        linkInfo: {
+            linkName: "contact",
+            linkText: "Kontakt"
+        },
+        title: {
+            format: "Header 3",
+            content: "Om Techcollege"
+        },
+        text: {
+            format: "Header 3",
+            content: "Her finder du alt hvad du skal bruge om os"
+        }
     },
-    {
-        linkName: "kontakt",
-        replaceWith: contact
-    }
 ]
